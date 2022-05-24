e4587ece9ff67532be6480c41d53313188db6c5f
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-05-24 17:22:50 -0300

    chore: install dependences and config database

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,5 @@
-import { getRepository, Repository } from 'typeorm';
+import { createQueryBuilder, getConnection, getRepository, Repository } from 'typeorm';
+import { Game } from '../../../games/entities/Game';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
@@ -13,8 +14,10 @@ export class UsersRepository implements IUsersRepository {
 
   async findUserWithGamesById({
     user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  }: IFindUserWithGamesDTO): Promise<User | undefined> {
+    const userWithGames = await this.repository.findOne(user_id);
+
+    return userWithGames;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
